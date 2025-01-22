f2ed3bd52ae195128ec94c81890456c723d80cf1
Giuseppe Garcia-Cancela <giu3337@icloud.com>
2025-01-22 14:23:04 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <p>{4/2}</p> 
+    </>
+  )
+}
+
+export default App
