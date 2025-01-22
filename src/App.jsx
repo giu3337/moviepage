798523c360f01bc6fdb2a63c140e4b8de3137598
Giuseppe Garcia-Cancela <giu3337@icloud.com>
2025-01-22 15:22:08 -0500

    2 con

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+
   
 
   return (
     <>
-      <p>{4/2}</p> 
+     <h1>App</h1>
     </>
   )
 }
