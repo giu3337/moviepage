9c52a31ea8f701cc44cc99330fd9dd37b336febf
Giuseppe Garcia-Cancela <giu3337@icloud.com>
2025-01-22 15:42:26 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
-import { useState } from 'react'
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
+
+const API_URL = "http://www.omdbapi.com/?apikey=86b8e854";
 
 function App() {
 
+  const searchMovies = async (title) => {
+    try {
+      const response = await fetch(`${API_URL}&s=${title}`);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
   
+  useEffect(() => {
+    searchMovies("batman");
+  }, []);
 
   return (
     <>
      <h1>App</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
